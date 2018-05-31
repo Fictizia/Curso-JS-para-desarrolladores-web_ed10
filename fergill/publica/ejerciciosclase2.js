17e1b864c87524961c5d92d6770093100821eede
fergill <javascript@zPro1.local>
2018-05-31 19:20:27 +0200

    5 primeros ejercicios
    5 primeros ejercicios

---
+++ fergill/publica/ejerciciosclase2.js
@@ -0,0 +1,55 @@
+var num1 = 1;
+
+var num2 = 2;
+
+var resultado = num1 + num2;
+
+console.log(resultado)
+
+console.log("*************")
+
+var num3 = 1;
+
+var num4 = 2;
+
+var resultado = num3 - num4;
+
+console.log(resultado)
+
+console.log("*************")
+
+var num5 = 1;
+
+var num6 = 2;
+
+var resultado = num5 / num6;
+
+console.log(resultado)
+
+console.log("*************")
+
+var num7 = 1;
+
+var num8 = 2;
+
+var resultado = num7 * num8;
+
+console.log(resultado)
+
+console.log("*************")
+
+var hombres = 2;
+
+var mujeres = 7;
+
+var total = 9;
+
+var porcentajeHombres = (hombres*100)/total;
+
+var porcentajeMujeres = (mujeres*100)/total;
+
+console.log(porcentajeHombres);
+
+console.log(porcentajeMujeres);
+
+console.log("*************")
