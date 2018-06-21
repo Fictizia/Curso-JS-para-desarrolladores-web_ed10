e105c73551adacd63b544ebbaf770f6cb7ffeec6
ulisesGascon <ulisesgascongonzalez@gmail.com>
2018-06-21 21:25:25 +0200

    Ya tenemos eventos! :boom:

--- ULISES/mini_proyecto/js/index.js
+++ ULISES/mini_proyecto/js/index.js
@@ -1,1 +1,16 @@
-console.log("Hola Mundo!")
+console.log("Hola Mundo!");
+
+/*Yay! Eventos!*/
+document.getElementById("nombreArtista").addEventListener("keyup", function(event){
+	if(event.keyCode === 13){
+		var nombrePelicula = document.getElementById("nombreArtista").value.trim();
+		console.log("Texto actual:", nombrePelicula)
+	}
+})
+
+
+
+// Objetivo: Sacar lo que hace el usuario en el teclado con el input
+// Secundario: Filtrar cuando hace enter y cuando no...
+
+
